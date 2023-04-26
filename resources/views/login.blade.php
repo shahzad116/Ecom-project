507708b350621e4129430e577c89801420848c61
shahzad <shahzadnazar116@gmail.com>
2023-04-26 11:48:55 +0400

    login done

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -6,11 +6,12 @@
             <div id="login-row" class="row justify-content-center align-items-center">
                 <div id="login-column" class="col-md-6">
                     <div id="login-box" class="col-md-12">
-                        <form id="login-form" class="form" action="" method="post">
+                        <form id="login-form" class="form" action="login" method="post">
+                            @csrf
                             <h3 class="text-center text-info">Login</h3>
                             <div class="form-group">
                                 <label for="username" class="text-info">Username:</label><br>
-                                <input type="text" name="username" id="username" class="form-control">
+                                <input type="email" name="email" id="email" class="form-control">
                             </div>
                             <div class="form-group">
                                 <label for="password" class="text-info">Password:</label><br>
@@ -22,7 +23,7 @@
                                 <input type="submit" name="submit" class="btn btn-info btn-md" value="submit">
                             </div>
                             <div id="register-link" class="text-right">
-                                <a href="#" class="text-info">Register here</a>
+                                <a href="#" class="text-info">Login</a>
                             </div>
                         </form>
                     </div>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,10 +16,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::get('/', function (){
-
+Route::get('/login', function (){
     return view('login');
 });
+
+Route::post('/login', [UserController::class,'login']);
+Route::get('/', [ProductController::class,'index']);
 
 
 // Route::get('/', function () {
